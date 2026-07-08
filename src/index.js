1efb8226bfcd6919be657312ae6844203eacd976
alis-g <ph0b0sch338@gmail.com>
2026-07-08 14:07:36 +0300

    set up handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', engine())
+
+app.set('view engine', 'hbs')
+app.set('views', './src/views');
+
+
 app.get('/',(req, res) => {
-    res.send('hellllo ddarkness')
+    res.render('home', {layout: false});
 })
 
 app.listen(2000, () => console.log('Server is listening on  http://localhost:2000...'));
